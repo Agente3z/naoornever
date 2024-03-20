4204d28c74f2bb29e5b01fac279c5c33c023d99e
agente3z <agente3z@kali.com>
2024-03-20 23:00:24 +0100

    quantita -> quantità

--- app/backend/main.py
+++ app/backend/main.py
@@ -21,12 +21,12 @@ def get():
     nome = request.args.get('nome')
     if nome:
         item = Inventory.query.filter_by(nome=nome).first_or_404()
-        result = {"categoria": item.categoria, "sottocategoria": item.sottocategoria, "nome": item.nome, "quantita": item.quantita, "materiali": item.materiali, "peso": item.peso, "prezzo": item.prezzo, "dimensioni": item.dimensioni, "foto": item.foto, "link": item.link}
+        result = {"categoria": item.categoria, "sottocategoria": item.sottocategoria, "nome": item.nome, "quantit": item.quantita, "materiali": item.materiali, "peso": item.peso, "prezzo": item.prezzo, "dimensioni": item.dimensioni, "foto": item.foto, "link": item.link}
         response = jsonify(result)
         return response
     else:
         inventory = Inventory.query.order_by(Inventory.categoria, Inventory.sottocategoria, Inventory.nome).all()
-        result = [{"categoria": item.categoria, "sottocategoria": item.sottocategoria, "nome": item.nome, "quantita": item.quantita, "materiali": item.materiali, "peso": item.peso, "prezzo": item.prezzo, "dimensioni": item.dimensioni, "foto": item.foto, "link": item.link} for item in inventory]
+        result = [{"categoria": item.categoria, "sottocategoria": item.sottocategoria, "nome": item.nome, "quantità": item.quantita, "materiali": item.materiali, "peso": item.peso, "prezzo": item.prezzo, "dimensioni": item.dimensioni, "foto": item.foto, "link": item.link} for item in inventory]
         response = jsonify(result)
         return response
 
@@ -42,7 +42,7 @@ def headers():
     headers = [{"name": "categoria", "type": "select", "selectOptions": categorie},
                 {"name": "sottocategoria", "type": "select", "selectOptions": sottocategorie},
                 {"name": "nome", "type": "text"},
-                {"name": "quantita", "type": "number"},
+                {"name": "quantità", "type": "number"},
                 {"name": "materiali", "type": "text"},
                 {"name": "peso", "type": "number"},
                 {"name": "prezzo", "type": "number"},
@@ -74,14 +74,14 @@ def getSottocategorie():
 @app.post('/add')
 @cross_origin()
 def add():
-    for i in ["categoria", "sottocategoria", "nome", "quantita", "materiali", "peso", "prezzo", "dimensioni", "foto", "link"]:
+    for i in ["categoria", "sottocategoria", "nome", "quantità", "materiali", "peso", "prezzo", "dimensioni", "foto", "link"]:
         if i not in request.form:
             response = jsonify(f"Missing parameter {i}")
             return response
     categoria = request.form.get('categoria')
     sottocategoria = request.form.get('sottocategoria')
     nome = request.form.get('nome')
-    quantita = request.form.get('quantita')
+    quantita = request.form.get('quantità')
     materiali = request.form.get('materiali')
     peso = request.form.get('peso')
     prezzo = request.form.get('prezzo')
@@ -158,7 +158,7 @@ def control_get():
         except:
             foto = ""
             quantita = 0
-        result.append({"id": i.id, "nome": i.nome, "conferma": i.conferma, "timestamp": i.timestamp, "foto": foto, "quantita": quantita})
+        result.append({"id": i.id, "nome": i.nome, "conferma": i.conferma, "timestamp": i.timestamp, "foto": foto, "quantità": quantita})
     response = jsonify(result)
     return response
 
